266d96f19a9f76516760ac128a4bc0ab0e64e852
Mattias Danielsson <matdanie@kth.se>
2013-02-06 16:16:52 +0100

    Added doc comment to pdfmaker

--- pdfmaker.py
+++ pdfmaker.py
@@ -6,29 +6,35 @@ from subprocess import call
 
 
 class PDFMaker:
-	
-	def make(self, name, paths, tmpdir):
-		"""Makes a pdf out of the files in the list paths"""
-		
-		
-		#PS stage
-		div = " "
-		src_list = div.join(paths)
-		
-		ps_target = "%s/%s.ps" % (tmpdir, name)
-	
-		call("""a2ps %s -2 -C -T 4 --footer="AutoGit2PDF by Mattias Danielsson" --toc -g -o %s""" % (src_list, ps_target), shell=True)
-		
-		#make pdf
-		pdf_target = "%s/%s.pdf" % (tmpdir, name)
-		
-		call("ps2pdf %s %s" % (ps_target, pdf_target), shell=True)
-		
-		#remove ps
-		os.unlink(ps_target)
+    
+    def make(self, name, paths, dir):
+        """Makes a pdf out of the files in the list paths
+           name     The name of the file to generate
+           paths    An array of paths to the files to compound
+           dir      The dir to place the result. Will be used as tmp-dir for intermediary files
 
-		return pdf_target
-		 
-		
+           Returns  The path to the generated file
+        """
+        
+        
+        #PS stage
+        div = " "
+        src_list = div.join(paths)
+        
+        ps_target = "%s/%s.ps" % (tmpdir, name)
+    
+        call("""a2ps %s -2 -C -T 4 --footer="github.com/mattenrone/Git2PDF" --toc -g -o %s""" % (src_list, ps_target), shell=True)
+        
+        #make pdf
+        pdf_target = "%s/%s.pdf" % (tmpdir, name)
+        
+        call("ps2pdf %s %s" % (ps_target, pdf_target), shell=True)
+        
+        #remove ps
+        os.unlink(ps_target)
+
+        return pdf_target
+         
+        
 
 
